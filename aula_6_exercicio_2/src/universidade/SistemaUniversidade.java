f76594d568cee49986f244ad201848d4e03df99f
MarksAurelio <marksaurelio@yahoo.com>
2025-06-13 23:09:08 -0300

    exercise update

--- aula_6_exercicio_2/src/universidade/SistemaUniversidade.java
+++ aula_6_exercicio_2/src/universidade/SistemaUniversidade.java
@@ -1,3 +1,14 @@
+/* Em uma universidade tem dois tipos de Funcionários: Diretores ou Professores. Um professor tem nome, matrícula, cpf, salário, ano de admissão. Em professor deve ter um método getImpostoDeRenda, que deverá retornar o imposto de renda do professor (20% do seu salário). Um diretor tem nome, matrícula, cpf, salário e tempo de casa. Em Diretor também tem o método para obter o imposto de renda. (23% do seu salário).
+
+Pegue tudo que for comum às entidades e coloque em uma classe Funcionário, inclusive o método getImpostoDeRenda, para que ele possa ser sobrescrito pelas entidades filhas.
+
+Não é aceito funcionário repetido nesta universidade. Use sobrescrita do equals para auxiliar nesta empreitada e para apresentação use o toString .
+
+Para criar um funcionário, deve ser passada a matrícula no construtor obrigatoriamente.
+
+Trabalhe com um ÚNICO ArrayList de Funcionário.
+
+Receba os dados de vários funcionários e apresente a média do imposto de renda e o total do imposto de renda. */
 package universidade;
 
 import java.util.ArrayList;
